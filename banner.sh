dc09aa08484e778ef44be8eaccbba3c8dfb82654
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2022-11-10 19:40:12 +0530

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -7,6 +7,17 @@ echo ""
 cd usr
 echo ""
 cd etc
-echo ""
-nano
 
+
+rm -rf bash.bashrc
+echo "" 
+touch bash.bashrc
+
+echo "neofetch --ascii_distro kali linux
+
+PS1='\033[01;34m\]└─\[\033[01;32m\]root\[\033[01;34m\]_\[\033[01;31m\]@D3V1L\[\033[00;34m\]\[\033[01;34m\]$\[\033[01;32m'" >bash.bashrc
+
+echo "" 
+pkg install neofetch
+
+exit
