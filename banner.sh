f032403f4bc128c2afa24fa0e416a679f5f20ec6
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2022-11-11 07:40:04 +0530

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,27 @@
+                         
+clear
+termux-setup-storage
+
+
+echo ""
+cd
+
+echo ""
+
+cd /data/data/com.termux/files/usr/etc
+echo ""
+rm -rf bash.bashrc
+
+echo ""
+touch bash.bashrc
+echo ""
+neofetch --ascii_distro kali linux
+
+PS1='\033[01;34m\]└─\[\033[01;32m\]root\[\033[01;34m\]_\[\033[01;31m\' >bash.bashrc
+
+echo ""
+pkg install neofetch
+
+echo "SUCCESSFULLY INSTALLED PLEASE EXIT FOR RESULT"
+
+exit
